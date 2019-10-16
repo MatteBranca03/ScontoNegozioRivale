05432efa2185be8fd02eafcea8344521e76074d2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:59:13 +0200

    Add project files.

---
+++ ScontoNegozioRivale/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScontoNegozioRivale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
