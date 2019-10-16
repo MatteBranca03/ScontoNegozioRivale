d7e2d888e1d7b4cf8a98ada9ff800d454e4428b7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:24:13 +0200

    cambiamenti

--- ScontoNegozioRivale/Program.cs
+++ ScontoNegozioRivale/Program.cs
@@ -6,7 +6,30 @@ namespace ScontoNegozioRivale
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("totale speso?");
+            double totale = double.Parse(Console.ReadLine());
+            double sconto;
+            double sconto1;
+            double sconto2;
+            double scontato;
+            double spesainpiu;
+            if (totale <= 300)
+            {
+                sconto = totale / 100 * 10;
+                scontato = totale - sconto;
+                Console.WriteLine($"l'importo da pagare è {scontato}");
+            }
+            else if (totale > 300)
+            {
+                sconto1 = 300 / 100 * 10;
+                spesainpiu = totale - 300;
+                sconto2 = spesainpiu / 100 * 20;
+                sconto = sconto1 + sconto2;
+                scontato = totale - sconto;
+                Console.WriteLine($"l'importo da pagare è {scontato}");
+            }
+            Console.ReadLine();
         }
+        
     }
 }
